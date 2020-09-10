ff6ed5f61d52ea249ba0bd337b1863842b0e9e0c
Mauricio Zavala <69006735+zpmauricio@users.noreply.github.com>
2020-09-09 20:31:14 -0500

    new attempt to fix the nullpointerexception on viewPager

--- app/src/main/java/net/it96/enfoque/MainActivity.kt
+++ app/src/main/java/net/it96/enfoque/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.viewpager.widget.ViewPager
 import kotlinx.android.synthetic.main.fragment_list.*
 import net.it96.enfoque.fragments.adapters.ViewPagerAdapter
 import net.it96.enfoque.fragments.list.ListFragment
@@ -12,11 +13,14 @@ import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var viewPager: ViewPager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
         val navController = navHostFragment.navController
+        viewPager = findViewById(R.id.viewPager)
         setupActionBarWithNavController(navController)
         setUpTabs()
         Timber.i("MZP onCreate")
@@ -32,8 +36,8 @@ class MainActivity : AppCompatActivity() {
         fragmentAdapter.addFragment(ListFragment(), "Projects")
 //        adapter.addFragment(TodayFragment(), "Favourites")
 //        adapter.addFragment(SettingsFragment(), "Settings")
-        view_pager!!.adapter = fragmentAdapter
-        tabs.setupWithViewPager(view_pager!!)
+        viewPager.adapter = fragmentAdapter
+        tabs.setupWithViewPager(viewPager)
 //
 //        tabs.getTabAt(0)!!.setIcon(R.drawable.ic_baseline_home_24)
     }
